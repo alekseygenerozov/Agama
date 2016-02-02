784edc2b4671f56e33bff00128ddac66612b392a
Eugene <eugvas@lpi.ru>
2016-02-02 19:09:17 +0000

    Added transformation from action/angle to position/velocity for Isochrone

--- src/actions_isochrone.cpp
+++ src/actions_isochrone.cpp
@@ -5,6 +5,38 @@
 
 namespace actions{
 
+/// solve  phase = eta - ecc * sin(eta)  for eta (eccentric anomaly)
+/// store eta, its sin and cos in output arguments
+static void solveKepler(double ecc, double phase, double &eta, double &sineta, double &coseta)
+{
+    const double EPS = 1e-10;  // accuracy
+    if(ecc < EPS) {
+        eta    = phase;
+        sineta = sin(eta);
+        coseta = cos(eta);
+        return;
+    }
+    bool signeta = phase > M_PI;
+    // initial guess
+    eta = 0.5*M_PI + (M_PI/8)/ecc * 
+        (sqrt(M_PI*M_PI + (ecc + (signeta ? 1.5*M_PI-phase : phase-0.5*M_PI)) * 16*ecc ) - M_PI);
+    if(signeta)
+        eta = 2*M_PI - eta;
+    double deltaeta = 0;
+    int niter = 0;
+    do {  // Newton's method
+        sineta    = sin(eta);
+        coseta    = cos(eta);
+        double f  = eta - ecc * sineta - phase;
+        double df = 1.  - ecc * coseta;
+        deltaeta  = -f/df;
+        // refinement using second derivative (thanks to A.Gurkan)
+        deltaeta  = -f / (df + 0.5 * deltaeta * ecc * sineta);
+        eta      += deltaeta;
+        niter++;
+    } while(fabs(deltaeta) > EPS && niter<42);
+}
+    
 Actions actionsIsochrone(
     const double M, const double b,
     const coord::PosVelCyl& point)
@@ -30,19 +62,19 @@ ActionAngles actionAnglesIsochrone(
     double L    = Ltotal(point);
     double Lb   = sqrt(L*L + 4*M*b);
     double LLb  = 0.5 * (1 + L/Lb);
-    double E    = -M / (b+rb) + 0.5 * (pow_2(point.vr) + pow_2(point.vtheta) + pow_2(point.vphi));
-    double sq2E = sqrt(-2*E);
+    double twoE = -2*M / (b+rb) + pow_2(point.vr) + pow_2(point.vtheta) + pow_2(point.vphi);
+    double sq2E = sqrt(-twoE);
     aa.Jphi     = coord::Lz(pointCyl);
     aa.Jz       = L - fabs(aa.Jphi);
     aa.Jr       = M / sq2E - Lb * LLb;
-    double M2Eb = M + 2*E * b;
-    double ecc  = sqrt(1 + 2*E * pow_2(L / M2Eb)); // eccentricity
-    double fac1 =  M2Eb * (1+ecc) / (sq2E * L);    // sqrt( (1+ecc) / (1-ecc) );
-    double fac2 = (M2Eb * (1+ecc) - 4*E*b) / (sq2E * Lb);
+    double M2Eb = M + twoE * b;
+    double ecc  = sqrt(1 + twoE * pow_2(L / M2Eb)); // eccentricity
+    double fac1 =  M2Eb * (1+ecc) / (sq2E * L);     // sqrt( (1+ecc) / (1-ecc) );
+    double fac2 = (M2Eb * (1+ecc) - 2*twoE*b) / (sq2E * Lb);
     // below are quantities that depend on position along the orbit
     double k1   = sq2E * point.r * point.vr;
-    double k2   = M + 2*E * rb;
-    double eta  = atan2(k1, k2);
+    double k2   = M + twoE * rb;
+    double eta  = atan2(k1, k2);  // eccentric anomaly
     double sineta     = k1 / sqrt(k1*k1 + k2*k2);  // sin(eta)
     double tanhalfeta = -k2/k1 + 1/sineta;         // tan(eta/2)
     double psi  = atan2(pointCyl.z * L,  -pointCyl.R * point.vtheta * point.r);
@@ -51,14 +83,61 @@ ActionAngles actionAnglesIsochrone(
     aa.thetaz   = math::wrapAngle(psi + LLb * (aa.thetar - (eta<0 ? 2*M_PI : 0))
                 - atan(fac1 * tanhalfeta) - atan(fac2 * tanhalfeta) * L/Lb );
     aa.thetaphi = math::wrapAngle(point.phi + chi + math::sign(aa.Jphi) * aa.thetaz);
-    if(aa.Jz == 0)
+    if(aa.Jz == 0)  // in this case the value of theta_z is meaningless
         aa.thetaz = 0;
     if(freq) {
-        freq->Omegar   = -2*E * sq2E / M;
+        freq->Omegar   = -twoE * sq2E / M;
         freq->Omegaz   = freq->Omegar * LLb;
         freq->Omegaphi = math::sign(aa.Jphi) * freq->Omegaz;
     }
     return aa;
+}
+
+coord::PosVelCyl mapIsochrone(
+    const double M, const double b,
+    const ActionAngles& aa, Frequencies* freq)
+{
+    double L    = aa.Jz + fabs(aa.Jphi);
+    double Lb   = sqrt(L*L + 4*M*b);
+    double LLb  = 0.5 * (1 + L/Lb);
+    double sini = sqrt(1 - pow_2(aa.Jphi / L));  // l in A9 - sin of inclination angle
+    double twoE = -pow_2(M / (aa.Jr + Lb*LLb));
+    double sq2E = sqrt(-twoE);
+    double M2Eb = M + twoE * b;
+    double frqr = -twoE * sq2E / M;  // Omega_r
+    double frqz = frqr * LLb;        // Omega_z
+    double ecc  = sqrt(1 + twoE * pow_2(L / M2Eb)); // eccentricity
+    double fac1 =  M2Eb * (1+ecc) / (sq2E * L);     // sqrt( (1+ecc) / (1-ecc) );
+    double fac2 = (M2Eb * (1+ecc) - 2*twoE*b) / (sq2E * Lb);
+    // quantities below depend on angles
+    double eta, sineta, coseta;  // psi in A12; will be computed by the following routine
+    solveKepler(ecc * M2Eb/M, aa.thetar, eta, sineta, coseta);
+    double r    = -M2Eb/twoE * sqrt( (1 - ecc * coseta) * (1 - ecc * coseta - 2*twoE*b/M2Eb) );
+    double vr   =  M2Eb/sq2E / r * ecc * sineta;
+    double tanhalfeta = sineta / (1 + coseta);
+    double beta = atan(fac1 * tanhalfeta) + atan(fac2 * tanhalfeta) * L/Lb;
+    double psi  = aa.thetaz - LLb * (aa.thetar - (eta>M_PI ? 2*M_PI : 0)) + beta;  // chi in A14
+    double tanhalfpsi = tan(0.5*psi);
+    double sinpsi     = 2 * tanhalfpsi / (1 + pow_2(tanhalfpsi));
+    double costheta   = sini * sinpsi;              // z/r
+    double sintheta   = sqrt(1 - pow_2(costheta));  // R/r is always non-negative
+    coord::PosVelCyl point;
+    point.R  = r * sintheta;
+    point.z  = r * costheta;
+    double vtheta = -L * sini * (1 - sinpsi * tanhalfpsi) / point.R;
+    point.vR = vr * sintheta + vtheta * costheta;
+    point.vz = vr * costheta - vtheta * sintheta;
+    point.vphi = aa.Jphi / point.R;
+    double chi = atan2(costheta * point.vphi, vtheta) + M_PI;
+    point.phi  = math::wrapAngle(aa.thetaphi - chi - aa.thetaz * math::sign(aa.Jphi));
+    point.phi  = math::wrapAngle(aa.thetaphi -
+        (aa.Jz != 0 ? aa.thetaz+chi : aa.thetaz-psi) * math::sign(aa.Jphi) );
+    if(freq) {
+        freq->Omegar   = frqr;
+        freq->Omegaz   = frqz;
+        freq->Omegaphi = math::sign(aa.Jphi) * frqz;
+    }
+    return point;
 }
 
 }  // namespace actions
--- tests/test_isochrone.cpp
+++ tests/test_isochrone.cpp
@@ -14,6 +14,11 @@
 #include <fstream>
 #include <cmath>
 
+#define TEST_OLD_TORUS
+#ifdef TEST_OLD_TORUS
+#include "torus/Toy_Isochrone.h"
+#endif
+
 bool test_isochrone(const coord::PosVelCyl& initial_conditions)
 {
     const bool output = false; // whether to write a text file
@@ -30,19 +35,31 @@ bool test_isochrone(const coord::PosVelCyl& initial_conditions)
     actions::ActionAngles aaI, aaF;
     actions::Actions acS;
     actions::Frequencies frI, frF;
+    math::Averager statfrIr, statfrIz;
     actions::Angles aoldF(0,0,0), aoldI(0,0,0);
     bool anglesMonotonic = true;
+    bool reversible = true;
     std::ofstream strm;
     if(output)
         strm.open("test_isochrone.dat");
-    double ifd = 1e-5;
+    double ifd = 1e-4;
+#ifdef TEST_OLD_TORUS
+    Torus::IsoPar toypar;
+    toypar[0] = sqrt(M);
+    toypar[1] = sqrt(b);
+    toypar[2] = Lz(initial_conditions);
+    Torus::ToyIsochrone toy(toypar);
+#endif
     for(size_t i=0; i<traj.size(); i++) {
+        traj[i].phi = math::wrapAngle(traj[i].phi);
         aaI = actions::actionAnglesIsochrone(M, b,  traj[i], &frI);
         aaF = actions::actionAnglesAxisymFudge(pot, traj[i], ifd, &frF);
-        acS = actions::actionsSpherical(pot, traj[i]);
+        acS = actions::actionsSpherical(pot, traj[i]);        
         statI.add(aaI);
         statF.add(aaF);
         statS.add(acS);
+        statfrIr.add(frI.Omegar);
+        statfrIz.add(frI.Omegaz);
         actions::Angles anewF, anewI;
         anewF.thetar   = math::unwrapAngle(aaF.thetar,   aoldF.thetar);
         anewF.thetaz   = math::unwrapAngle(aaF.thetaz,   aoldF.thetaz);
@@ -56,6 +73,29 @@ bool test_isochrone(const coord::PosVelCyl& initial_conditions)
             anewI.thetaphi >= aoldI.thetaphi && anewF.thetaphi >= aoldF.thetaphi);
         aoldF = anewF;
         aoldI = anewI;
+        coord::PosVelCyl pp = mapIsochrone(M, b, aaI);  // inverse transformation
+        reversible &= equalPosVel(pp, traj[i], epsd);
+#ifdef TEST_OLD_TORUS
+        coord::PosVelSph ps(toPosVelSph(traj[i]));
+        Torus::PSPT pvs;
+        pvs[0]=ps.r; pvs[1]=M_PI/2-ps.theta; pvs[2]=ps.phi;
+        pvs[3]=ps.vr; pvs[4]=-ps.vtheta*ps.r; pvs[5]=ps.vphi*traj[i].R;
+        Torus::PSPT aaT = toy.Backward3D(pvs); // (J_r,J_z,J_phi,theta_r,theta_z,theta_phi)
+        Torus::PSPT pvi = toy.Forward3D(aaT);
+        reversible &= 
+        math::fcmp(pvs[0], pvi[0], eps) == 0 && math::fcmp(pvs[1], pvi[1], eps) == 0 &&
+        math::fcmp(pvs[2], pvi[2], eps) == 0 && math::fcmp(pvs[3]+1, pvi[3]+1, eps) == 0 &&
+        math::fcmp(pvs[4]+1, pvi[4]+1, eps) == 0 && math::fcmp(pvs[5]+1, pvi[5]+1, eps) == 0;
+        reversible &=
+        math::fcmp(aaT[0]+1, aaI.Jr+1, eps) == 0 &&  // ok when Jr<<1
+        math::fcmp(aaT[1], aaI.Jz, eps) == 0 &&
+        math::fcmp(aaT[2], aaI.Jphi, eps) == 0 &&
+        (aaI.Jr<epsd || math::fcmp(aaT[3], aaI.thetar, eps) == 0) &&
+        (aaI.Jz==0 || math::fcmp(math::wrapAngle(aaT[4]+1), math::wrapAngle(aaI.thetaz+1), eps) == 0) &&
+        math::fcmp(aaT[5], aaI.thetaphi, eps) == 0;
+        if(!reversible)
+            std::cout << aaT <<'\t' <<aaI << '\n';
+#endif
         if(output) {
             strm << i*timestep<<"   "<<traj[i].R<<" "<<traj[i].z<<"   "<<
                 aaI.thetar<<" "<<aaI.thetaz<<" "<<aaI.thetaphi<<"  "<<
@@ -73,6 +113,7 @@ bool test_isochrone(const coord::PosVelCyl& initial_conditions)
              fabs(statI.avg.Jr-statF.avg.Jr)<eps
           && fabs(statI.avg.Jz-statF.avg.Jz)<eps
           && fabs(statI.avg.Jphi-statF.avg.Jphi)<eps;
+    bool freq_ok = statfrIr.disp() < epsd && statfrIz.disp() < epsd;
     std::cout << "Isochrone"
     ":  Jr="  <<statI.avg.Jr  <<" +- "<<statI.disp.Jr<<
     ",  Jz="  <<statI.avg.Jz  <<" +- "<<statI.disp.Jz<<
@@ -86,8 +127,10 @@ bool test_isochrone(const coord::PosVelCyl& initial_conditions)
     ",  Jz="  <<statF.avg.Jz  <<" +- "<<statF.disp.Jz<<
     ",  Jphi="<<statF.avg.Jphi<<" +- "<<statF.disp.Jphi<< (dispF_ok?"":" \033[1;31m**\033[0m")<<
     (compareIF?"":" \033[1;31mNOT EQUAL\033[0m")<<
+    (reversible?"":" \033[1;31mNOT INVERTIBLE\033[0m")<<
+    (freq_ok?"":" \033[1;31mFREQS NOT CONST\033[0m")<<
     (anglesMonotonic?"":" \033[1;31mANGLES NON-MONOTONIC\033[0m")<<'\n';
-    return dispI_ok && dispS_ok && dispF_ok && compareIF && anglesMonotonic;
+    return dispI_ok && dispS_ok && dispF_ok && compareIF && freq_ok && reversible && anglesMonotonic;
 }
 
 int main()
@@ -96,7 +139,7 @@ int main()
     ok &= test_isochrone(coord::PosVelCyl(1.0, 0.3, 1.1, 0.1, 0.4, 0.1));  // ordinary case
     ok &= test_isochrone(coord::PosVelCyl(1.0, 0.0, 2.0, 1.0, 0.0, 0.5));  // Jz==0
     ok &= test_isochrone(coord::PosVelCyl(1.0, 0.0, 3.0, 0.0, 0.21,0.9));  // Jr small
-    //ok &= test_isochrone(coord::PosVelCyl(1.0, 0.0, 2.0, 5.0, 1.0, 1e-4));  // Jphi small
+    ok &= test_isochrone(coord::PosVelCyl(1.0, 0.0, 2.0, 0.6, 1.0, 1e-4)); // Jphi small
     if(ok)
         std::cout << "\033[1;32mALL TESTS PASSED\033[0m\n";
     return 0;
